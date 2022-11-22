c3d5ec40a4b15fcb2bd0d12f1cf1e81bf8fddeab
Aleks_K <korobov_a_u@mail.ru>
2022-11-22 13:35:07 +0500

    готова четвертая задача

---
+++ exercise4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+int num = 0;
+while(num<=x-2) 
+{
+    num = num+2;
+    Console.WriteLine(num);
+}
