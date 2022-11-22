6155a3c4115c68354375d185a61195c5c252a3ec
Aleks_K <korobov_a_u@mail.ru>
2022-11-22 15:21:20 +0500

    15 задача

---
+++ 11/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число дня недели от 1 до 7: ");
+
+int num = int.Parse(Console.ReadLine());
+
+if((num == 1)||(num == 2)||(num == 3)||(num == 4)||(num == 5))
+        {
+            Console.WriteLine("Нет");
+        }
+if((num == 6)||(num == 7))
+        {
+            Console.WriteLine("Да");
+        }
