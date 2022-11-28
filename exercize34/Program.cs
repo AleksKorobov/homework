115ff066597a141d893910cded5707c735298535
Aleks_K <korobov_a_u@mail.ru>
2022-11-28 13:12:16 +0500

    задание 34  урок4

---
+++ exercize34/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+void FiilArrayRandomeNum(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 0)
+        count++;
+    }
+    return count;
+}
+int[] a = new int[10];
+FiilArrayRandomeNum(a);
+PrintArray(a);
+Console.WriteLine(CountEvenNumbers(a));
