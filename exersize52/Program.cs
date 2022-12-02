0d576e13a452ca047a12bec41e98f42d425d407d
Aleks_K <korobov_a_u@mail.ru>
2022-12-02 19:36:56 +0500

    Задача 52

---
+++ exersize52/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.*/
+int GetNum(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10) ;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine("");
+    }
+}
+Console.Clear();
+int rows = GetNum("Введите количество строк"!);
+int cols = GetNum("Введите количество столбцов"!);
+int[,] numbers = new int[rows, cols];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+Console.WriteLine("Среднее арифметическое каждого столбца:");
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / rows;
+    Console.Write(avarage + ";  ");
+}
