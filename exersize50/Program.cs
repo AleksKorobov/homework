6718c381cf1b35042c95cd57dc74b28d2d7549b2
Aleks_K <korobov_a_u@mail.ru>
2022-12-02 19:16:28 +0500

    Задача 52

---
+++ exersize50/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
+int GetNum(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(0, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}\t");
+        }   
+        Console.WriteLine(""); 
+    }
+}
+Console.Clear();
+int n = GetNum("Введите номер строки"!);
+int m = GetNum("Введите номер столбца"!);
+int[,] numbers = new int[4, 4];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+Console.WriteLine();
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {m} столбца и {n} строки равно {numbers[n-1,m-1]}");
+}
