93922c475d74d52e0c2f9c1d673398af96f3601b
Aleks_K <korobov_a_u@mail.ru>
2022-12-04 20:54:04 +0500

    Задача 56

---
+++ exersize56/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.*/
+
+int GetNum(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10) ;
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine("");
+    }
+}
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+Console.Clear();
+int rows = GetNum("Введите количество строк"!);
+int cols = GetNum("Введите количество столбцов"!);
+int[,] numbers = new int[rows, cols];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(numbers, 0);
+for (int i = 1; i < numbers.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(numbers, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой элементов равных {sumLine}");
+Console.WriteLine();
+
+
