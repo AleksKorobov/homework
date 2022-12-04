50dcaeebde221d0fcb9f9ca7341c4f440f9dcbdc
Aleks_K <korobov_a_u@mail.ru>
2022-12-04 16:05:07 +0500

    Задача 54

---
+++ exersize54/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.*/
+
+int GetNum(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10) ;
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine("");
+    }
+}
+void SortDecay(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Clear();
+int rows = GetNum("Введите количество строк"!);
+int cols = GetNum("Введите количество столбцов"!);
+int[,] numbers = new int[rows, cols];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+SortDecay(numbers);
+Console.WriteLine("Массив отсортированный по строкам:"!);
+PrintArray(numbers);
