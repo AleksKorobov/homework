328fef1de06c0401ec42f7efde1b3495740f7c23
Aleks_K <korobov_a_u@mail.ru>
2022-12-05 13:37:04 +0500

    Задача 64

---
+++ exersize64/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+
+Console.Clear();
+int n = ReadInt("Введите число N = ");
+int m = 1;
+NumToLow(n, m);
+
+
+void NumToLow(int n, int m)
+{
+    if (m > n)
+    {
+        return;
+    }
+    else
+    {
+        NumToLow(n, m + 1);
+        Console.Write(m + " ");
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
